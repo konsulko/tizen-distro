5f49d050ab574302aeed3f3968ddf56eca0c270e
Ronan Le Martret <ronan@fridu.net>
2014-10-03 14:57:49 +0200

    meta-tizen: Fix ail start up
    Change-Id: If074dba66aebec6247c905b0b13e224db9931876
    (From meta-tizen rev: a1df6368d3125e2ba9bd67a4740fbdb0a7ffd640)
    
    Signed-off-by: Ronan Le Martret <ronan@fridu.net>
    Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>

--- meta-tizen/recipes-tizen/Modello-Installer/Modello-Installer/modello-installer-postinst.sh
+++ meta-tizen/recipes-tizen/Modello-Installer/Modello-Installer/modello-installer-postinst.sh
@@ -3,18 +3,29 @@
 source /etc/tizen-platform.conf
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/5000/dbus/user_bus_socket"
 
+export MODELLO_INSTALL_USER="app"
+export MODELLO_COMMON_DIR="/usr/share/Modello/Common"
+
+export PATH="${PATH}:/bin"
+
 for list in $(find $TZ_SYS_APP_PREINSTALL -name "Modello*")
 do
-    #XWalk requires you not be root to install files
-    echo "Installing $list"
-    /bin/su app -c "xwalkctl -i $list"
+#XWalk requires you not be root to install files
+        echo "Installing $list"
+        su ${MODELLO_INSTALL_USER} -c "xwalkctl -i $list"
 done
 
-for list2 in $(ls -d $TZ_SYS_HOME/app/.config/xwalk-service/applications/*/)
-do
-    /bin/su app -c "mkdir -p '$list2/css'"
-    /bin/su app -c "mkdir -p '$list2/js'"
-    /bin/su app -c "cp -r /opt/usr/apps/_common/js/services '$list2/js/'"
-    /bin/su app -c "cp -r /opt/usr/apps/_common/css/* '$list2/css/'"
-    /bin/su app -c "cp -r /opt/usr/apps/_common/icons '$list2/'"
+for list2 in $(ls -d $TZ_SYS_HOME/${MODELLO_INSTALL_USER}/.config/xwalk-service/applications/*/)
+do      
+        echo list2 $
+        su ${MODELLO_INSTALL_USER} -c "mkdir -p '$list2/css'"
+        su ${MODELLO_INSTALL_USER} -c "mkdir -p '$list2/js'"
+        su ${MODELLO_INSTALL_USER} -c "cp -fr ${MODELLO_COMMON_DIR}/js/services '$list2/js/'"
+        su ${MODELLO_INSTALL_USER} -c "cp -fr ${MODELLO_COMMON_DIR}/css/* '$list2/css/'"
+        su ${MODELLO_INSTALL_USER} -c "cp -fr ${MODELLO_COMMON_DIR}/icons '$list2/'"
+        
+        
 done
+
+find $TZ_SYS_HOME/${MODELLO_INSTALL_USER}/.config/xwalk-service/applications/ -exec chsmack -a "User" {} \;
+
--- meta-tizen/recipes-tizen/ail/ail_git.bb
+++ meta-tizen/recipes-tizen/ail/ail_git.bb
@@ -4,7 +4,7 @@ PRIORITY = "10"
 
 LIC_FILES_CHKSUM ??= "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=801f80980d171dd6425610833a22dbe6"
 
-SRC_URI += "git://review.tizen.org/platform/core/appfw/ail;tag=84d6c89b12efe4af20c21ced0e7775035b3d50e4;nobranch=1"
+SRC_URI += "git://review.tizen.org/platform/core/appfw/ail;tag=e252503f15cb8c3d3401178f926762498ab9aedb;nobranch=1"
 
 BBCLASSEXTEND += " native "
 
