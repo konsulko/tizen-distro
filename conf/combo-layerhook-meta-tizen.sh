7838d9ea5fa9b46f6ea5ccec5772462264364831
Patrick Ohly <patrick.ohly@intel.com>
2015-01-08 06:29:16 -0800

    tizen-distro: configure combo-layer
    Up to this point, the tizen-distro repository is identical to the poky
    master branch minus the "yocto" and "documentation" parts. It was created
    by checking out 081fddd3e464935e5f438a7686eb8f8856da6281 (the commit where
    "dizzy" was branched off from master) and stripping out these undesired files
    with: git filter-branch --prune-empty --tree-filter 'rm -rf *yocto* documentation' HEAD
    
    Then the last_revision properties for bitbake and openembedded-core were set
    based on the commit messages generated in poky by the combo-layer commit hook.
    Because these extra comments are useful, the same commit hooks are also
    used in tizen-distro.
    
    Now the repository is ready for importing the other components.

---
+++ conf/combo-layerhook-meta-tizen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Hook to add source component/revision info to commit message
+# Parameter:
+#   $1 patch-file
+#   $2 revision
+#   $3 reponame
+
+patchfile=$1
+rev=$2
+reponame=$3
+
+# Some patches in meta-tizen follow the "component: change" pattern,
+# some do not.  Even if used, the component can be misleading because
+# it appears in both meta-tizen (as .bbappend file) and other
+# components (as .bb file). Therefore always add the "meta-tizen"
+# prefix.
+sed -i -e "s#Subject: \[PATCH\] \(.*\)#Subject: \[PATCH\] meta-tizen: \1#" $patchfile
+
+sed -i -e "0,/^Signed-off-by:/s#\(^Signed-off-by:.*\)#\(From meta-tizen rev: $rev\)\n\n\1#" $patchfile
---
+++ conf/combo-layerhook-openembedded-core.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Hook to add source component/revision info to commit message
+# Parameter:
+#   $1 patch-file
+#   $2 revision
+#   $3 reponame
+
+patchfile=$1
+rev=$2
+reponame=$3
+
+sed -i -e "0,/^Signed-off-by:/s#\(^Signed-off-by:.*\)#\(From OE-Core rev: $rev\)\n\n\1#" $patchfile
